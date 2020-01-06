b5a67df72371b922f7950f87ffdfc73067982d7b
Roh Geonho <roh517@naver.com>
2020-01-06 14:07:21 +0900

    comments

--- lib/pages/Post/select.dart
+++ lib/pages/Post/select.dart
@@ -1,4 +1,5 @@
-import 'package:dynamic_list_view/DynamicListView.dart';
+import 'package:flutter_schooldev/pages/Post/ui_helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -89,10 +90,13 @@ class _ListPageState extends State<ListPage>{
                 itemCount: snapshot.data.length,
                 itemBuilder: (_, index){
 
-                  return ListTile(
-                    title: Text(snapshot.data[index].data['title']),
-                    onTap: () => navigateToDetail(snapshot.data[index]),
+                  return Card(
+                    child: ListTile(
+                      title: Text(snapshot.data[index].data['title']),
+                      onTap: () => navigateToDetail(snapshot.data[index]),
+                    ),
                   );
+
                 });
           }
         }),
@@ -122,9 +126,27 @@ class DetailPage extends StatefulWidget{
 }
 
 class _DatailPageState extends State<DetailPage>{
+  var comments_data;
+  void initState(){
+    comments_data = widget.post.data['comments'];
+    print(comments_data);
+    if(comments_data == null){
+      comments_data = [""];
+    }
+  }
+
+
   @override
   Widget build(BuildContext context){
-
+    Widget welcome_widget(){
+      return Container(
+        padding: EdgeInsets.all(15.0),
+        child: Text(
+          widget.post.data['title'],
+          style: TextStyle(fontSize: 28.0, color: Colors.black),
+        ),
+      );
+    }
     final welcome = Container(
       padding: EdgeInsets.all(15.0),
       
@@ -133,9 +155,32 @@ class _DatailPageState extends State<DetailPage>{
         style: TextStyle(fontSize: 28.0, color: Colors.black),
       ),
     );
-
+    Widget lorem_widget(){
+      return Container(
+        padding: EdgeInsets.fromLTRB(4,14,4,14),
+        decoration: BoxDecoration(
+            color: Color.fromRGBO(241, 241, 241, 0.8),
+            borderRadius: new BorderRadius.only(
+                topLeft: const Radius.circular(10.0),
+                topRight: const Radius.circular(10.0),
+                bottomRight: const Radius.circular(10.0),
+                bottomLeft:const Radius.circular(10.0)
+            )),
+        child: Align(
+            alignment: Alignment.topLeft,
+//      padding: EdgeInsets.all(20.0),
+            child: Text(
+              widget.post.data['content'],
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.black,
+              ),
+            )),
+      );
+    }
     final lorem = Container(
-      padding: EdgeInsets.all(15.0),
+      padding: EdgeInsets.fromLTRB(4,14,4,14),
       decoration: BoxDecoration(
           color: Color.fromRGBO(241, 241, 241, 0.8),
           borderRadius: new BorderRadius.only(
@@ -144,10 +189,94 @@ class _DatailPageState extends State<DetailPage>{
               bottomRight: const Radius.circular(10.0),
               bottomLeft:const Radius.circular(10.0)
           )),
+      child: Align(
+        alignment: Alignment.topLeft,
+//      padding: EdgeInsets.all(20.0),
       child: Text(
         widget.post.data['content'],
-        style: TextStyle(fontSize: 16.0, color: Colors.black),
-      ),
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 17.0,
+          color: Colors.black,
+        ),
+      )),
+    );
+
+    //Future _data;
+    Widget comments_widget(){
+      return Container(
+
+          height: 1000,
+          child: ListView.builder(
+            itemCount: comments_data.length,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(comments_data[index]),
+              );
+            },
+          )
+      );
+    }
+    final comments = Container(
+        height: 1000,
+        child: ListView.builder(
+          itemCount: comments_data.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(comments_data[index]),
+            );
+          },
+        )
+    );
+
+
+//    Flexible(
+//      child:ListView.builder(
+//          itemCount: commentsdata.length,
+//          itemBuilder: (_, index){
+//
+//            return Container(
+//              padding: EdgeInsets.all(10.0),
+//              margin: EdgeInsets.symmetric(vertical: 10.0),
+//              decoration: BoxDecoration(
+//                  borderRadius: BorderRadius.circular(10.0), color: Colors.grey),
+//              child: Column(
+//                crossAxisAlignment: CrossAxisAlignment.start,
+//                children: <Widget>[
+//                  Text(
+//                    '노건호',
+//                    style: TextStyle(fontWeight: FontWeight.bold),
+//                  ),
+//                  UIHelper.verticalSpaceSmall(),
+//                  Text('Test'),
+//                ],
+//              ),
+//            );
+//          }) ,
+//    );
+   // ),
+//    final comments = Container(
+//      padding: EdgeInsets.all(10.0),
+//      margin: EdgeInsets.symmetric(vertical: 10.0),
+//      decoration: BoxDecoration(
+//          borderRadius: BorderRadius.circular(10.0), color: Colors.green),
+//      child: Column(
+//        crossAxisAlignment: CrossAxisAlignment.start,
+//        children: <Widget>[
+//          Text(
+//            '노건호',
+//            style: TextStyle(fontWeight: FontWeight.bold),
+//          ),
+//          UIHelper.verticalSpaceSmall(),
+//          Text('Test'),
+//        ],
+//      ),
+//    );
+
+    final space = Container(
+      child:
+      UIHelper.verticalSpace(50.0)
     );
 
     return Scaffold(
@@ -156,16 +285,17 @@ class _DatailPageState extends State<DetailPage>{
       ),
       body: SingleChildScrollView(
           child: Container(
+//            height: ,
             width: MediaQuery.of(context).size.width,
             padding: EdgeInsets.all(30.0),
-            decoration: BoxDecoration(
+//            decoration: BoxDecoration(
 //              gradient: LinearGradient(colors: [
 //                Colors.white,
 //                Colors.lightGreenAccent,
 //              ]),
-            ),
+//            ),
             child: Column(
-              children: <Widget>[ welcome, lorem],
+              children: <Widget>[ welcome_widget(), lorem_widget(), space ,comments_widget()],
             ),
           )
       ),
